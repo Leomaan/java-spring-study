7cd0afc592ba8ef9311ed46aa34c1dae4915e2bd
Leomaan <leoman.cassio@hotmail.com>
2025-05-06 17:06:42 -0300

    Initial

---
+++ src/test/java/com/leoman/study_spring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoman.study_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
